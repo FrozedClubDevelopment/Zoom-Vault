c8890817432dad6db3c396d18cca78f39f3ac88c
Alex Avila <alex.ramon2004@gmail.com>
2020-10-01 22:05:16 -0500

    Initial commit

---
+++ src/main/java/club/frozed/zoom/ZoomVaultImplementation.java
@@ -0,0 +1,22 @@
+package club.frozed.zoom;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Ryzeon
+ * Project: Zoom-Vault
+ * Date: 1/10/2020 @ 22:03
+ */
+
+public class ZoomVaultImplementation extends JavaPlugin {
+
+    @Override
+    public void onDisable() {
+        super.onDisable();
+    }
+
+    @Override
+    public void onEnable() {
+        super.onEnable();
+    }
+}
